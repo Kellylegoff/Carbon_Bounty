17f1849b7fb8f0371d3917f75ff87a64aaeb2220
SABAH EL MOUHSINI <elmosabah@gmail.com>
2022-04-05 15:32:02 +0200

    usecase et customer controller

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,16 +1,34 @@
 class CustomersController < ApplicationController
+  before_action :set_customer, only: [:create, :show, :update, :destroy]
+
   def create
+    @customer.save
   end
 
   def show
+    @customer = Customer.find(params[:id])
   end
 
   def index
+    @customer = Customer.all
   end
 
   def update
+    set_usecase
   end
 
   def destroy
+    set_usecase
+    @customer.destroy
+  end
+
+  private
+
+  def set_usecase
+    @customer = Customer.find(params[:id])
+  end
+
+  def usecase_params
+    params.require(:customer).permit(:company_name, :address, :phone, :contact_first_name, :contact_last_name, :role, :company_type, :sector)
   end
 end
