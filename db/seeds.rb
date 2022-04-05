d09a6a4bec613b8c34ec5c46ea1bf9dd465b4972
kelly@mavana.earth <kelly@mavana.earth>
2022-04-05 17:20:07 +0200

    seedsiotproduct

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+iotproducts = Iotproduct.create([{product_name: 'Flexidry'},
+   {product_name: 'CO2 , TEMPERATURE, HUMIDITY AND BAROMETRIC PRESSURE
+    SENSOR FOR LORAWAN ®'},
+   {product_name: 'INDOOR AMBIANCE MONITOR INCLUDING CO₂, TVOC AND MOTION SENSOR FOR LoRaWAN®'},
+   {product_name: 'ANALOG OR DIGITAL SENSOR DEVICE FOR LoRaWAN®'},
+   {product_name: 'K-Hub'},])
