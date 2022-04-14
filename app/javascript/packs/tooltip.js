2213a16f4ed7220d38b9746436d2b1afb4a9ccff
Jonathan Leal <jonathanleal@msn.com>
2022-04-14 11:33:47 +0200

    tooltip fix

--- app/javascript/packs/tooltip.js
+++ app/javascript/packs/tooltip.js
@@ -1,11 +1,19 @@
 import tippy from 'tippy.js';
 import 'tippy.js/dist/tippy.css';
+import 'tippy.js/themes/light.css';
+
 $(() =>{
   tippy('.product_type', {
     content: "End node (objet / capteur standalone)",
+    theme: 'light',
+    appendTo: document.body,
+    arrow: true,
   });
   tippy('.product_eco', {
     content: "Score calculé en fonction des facteurs d'émission. \n Plus le score est élevé et plus la solution présente une performance environnementale intéressante.",
+    theme: 'light',
+    appendTo: document.body,
+    arrow: true,
   });
   // $('button').click(() => console.log('hi2'));
 
